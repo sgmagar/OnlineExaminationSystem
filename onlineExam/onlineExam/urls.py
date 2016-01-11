bdafa9bbf7b7cfd17d592f891e5aaa2ac078a7e3
Saurav Gharti <sp.gharti@gmail.com>
2016-01-11 14:38:17 +0545

    modified views.py for the home page

--- onlineExam/onlineExam/urls.py
+++ onlineExam/onlineExam/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from .views import *
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', home, name='home'),
--- onlineExam/onlineExam/views.py
+++ onlineExam/onlineExam/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404
 from django.core.urlresolvers import reverse
 
+
 from exam.models import *
+
+def home(request):
+	
+	context = {
+		"title":"Home"
+	}
+	return render(request,'home.html',context)
